6e84dd3173f097f26649ca6444e1657e36cf59f8
Kazune Furudo <f.kazune08031991@gmail.com>
2020-04-22 22:53:20 +0900

    Fix code

--- image_detect.py
+++ image_detect.py
@@ -6,17 +6,17 @@ color = (255, 255, 255)
 cap = cv2.VideoCapture(0)
 
 scale_factor = 1.1
-min_neighbors = 2
-min_size = (2, 2)
+min_neighbors = 1
+min_size = (200, 200)
 
 while True:
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     rect = cascade.detectMultiScale(gray, scaleFactor=scale_factor, minNeighbors=min_neighbors, minSize=min_size)
     if len(rect) > 0:
         for x, y, w, h in rect:
             cv2.rectangle(frame, (x, y), (x+w, y+h), color)
-    
+
     cv2.imshow('detected', frame)
     key = cv2.waitKey(1)
     if key == ord("q"):
